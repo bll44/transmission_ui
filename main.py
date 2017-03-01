f81f99ef7df0246fd1d9d6f236f0063c84e91041
Brady Latsha <Brady Latsha>
2017-02-28 23:38:54 -0500

    basic cherrypy setup. began creating layout with materialize. probably will not use template/dashboard

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from transmissionclient.transmissionclient import TransmissionClient
+import cherrypy
+import os
+import settings
 from pprint import pprint
 import urllib.parse
 
+tc = TransmissionClient('yards01', 9091)
+
+class TransmissionControl(object):
+
+    # @cherrypy.expose
+    def index(self):
+        pass
+        # return open(os.path.join(settings.html_dir, 'index.html'), 'r').read()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def generate(self):
+        torrents = tc.get_torrents()
+        torrent_list = []
+        for index, t in enumerate(torrents):
+            torrent = {
+                'id': t.id,
+                'name': t.name
+            }
+            torrent_list.append(torrent)
+        return torrent_list
 
 def open_client():
-    tc = TransmissionClient('yards01', 9091)
     url = 'http://releases.ubuntu.com/14.04/ubuntu-14.04.5-server-i386.iso.torrent?_ga=1.262345295.2111688684.1488167464'
     torrents = tc.get_torrents()
     for index, t in enumerate(torrents):
@@ -14,4 +37,19 @@ def open_client():
 
 
 if __name__ == '__main__':
-    open_client()
+    cherrypy.quickstart(TransmissionControl(), '/',
+                        {'/': {
+                            'tools.staticdir.root': settings.html_dir,
+                            'tools.staticdir.on': True,
+                            'tools.staticdir.dir': '',
+                            'tools.staticdir.index': 'templates/dashboard/index.html'
+                        },
+                        '/images': {
+                            'tools.staticdir.on': True,
+                            'tools.staticdir.dir': 'templates/dashboard/images'
+                        },
+                        '/styles.css': {
+                            'tools.staticfile.on': True,
+                            'tools.staticfile.filename': os.path.join(settings.html_dir,
+                                                                      'templates/dashboard/styles.css')
+                        }})
---
+++ settings.py
@@ -0,0 +1,6 @@
+"""
+Settings file to store general configuration data for the application
+"""
+import os
+
+html_dir = os.path.abspath(os.path.join(__file__, '..', 'html_files'))
